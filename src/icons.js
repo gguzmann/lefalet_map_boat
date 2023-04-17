74e16a7feb9bc05a2c5270bb7b5649cbd04045e4
gabusho92 <g.guzman.nunez@gmail.com>
2023-04-17 11:55:40 -0400

    load image

--- src/icons.js
+++ src/icons.js
@@ -9,8 +9,8 @@ export const iconLocation = L.icon({
 })
 
 export const customMarker = L.icon({
-  iconUrl: '/src/assets/pirate.png',
-  iconRetinaUrl: '/src/assets/pirate.png',
+  iconUrl: './pirate.png',
+  iconRetinaUrl: './pirate.png',
   iconSize: [60, 30]
 
 })
